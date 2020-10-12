6978c2ff0ed867afb29a1ad520971273663458ea
Edenik <Edenik5@gmail.com>
2020-10-12 22:32:36 +0300

    Project Setup and Coin Model

---
+++ lib/models/coin_model.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+class Coin extends Equatable {
+  final String name;
+  final String fullName;
+  final double price;
+
+  const Coin({
+    @required this.name,
+    @required this.fullName,
+    @required this.price,
+  });
+
+  //Todo: add ImageUrl
+
+  @override
+  List<Object> get props => [
+        name,
+        fullName,
+        price,
+      ];
+
+  @override
+  String toString() =>
+      'Coin { name: $name, fullname: $fullName, price: $price}';
+
+  factory Coin.fromJsom(Map<String, dynamic> json) {
+    return Coin(
+      name: json['CoinInfo']['Name'] as String,
+      fullName: json['CoinInfo']['FullName'] as String,
+      price: (json['RAW']['USD']['PRICE'] as num).toDouble(),
+    );
+  }
+}
